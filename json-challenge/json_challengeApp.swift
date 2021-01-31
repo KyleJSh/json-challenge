0d7c2cf7a5739b82a36cbb78cc39ff3d3ccc75ba
Kyle Sherrington <kylejsherrington@gmail.com>
2021-01-30 17:16:45 -0800

    Initial Commit

---
+++ json-challenge/json_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_challengeApp.swift
+//  json-challenge
+//
+//  Created by Kyle Sherrington on 2021-01-30.
+//
+
+import SwiftUI
+
+@main
+struct json_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
